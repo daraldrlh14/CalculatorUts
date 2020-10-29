a2de4858bf9236fd8fdf86e2e98145cc65a4c490
daraldrlh <50088698+daraldrlh@users.noreply.github.com>
2020-10-29 16:49:46 +0800

    update

--- app/src/main/java/com/daraldrlh/calculatoruts/MainActivity.kt
+++ app/src/main/java/com/daraldrlh/calculatoruts/MainActivity.kt
@@ -8,6 +8,8 @@ import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
