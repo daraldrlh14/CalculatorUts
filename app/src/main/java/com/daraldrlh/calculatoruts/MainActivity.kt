c43517786972a9924ccfa885626e17776d5fe050
daraldrlh <50088698+daraldrlh@users.noreply.github.com>
2020-11-03 10:18:22 +0800

    update

--- app/src/main/java/com/daraldrlh/calculatoruts/MainActivity.kt
+++ app/src/main/java/com/daraldrlh/calculatoruts/MainActivity.kt
@@ -14,42 +14,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Numbers
-        tvOne.setOnClickListener { appendOnExpresstion("1", true) }
-        tvTwo.setOnClickListener { appendOnExpresstion("2", true) }
-        tvThree.setOnClickListener { appendOnExpresstion("3", true) }
-        tvFour.setOnClickListener { appendOnExpresstion("4", true) }
-        tvFive.setOnClickListener { appendOnExpresstion("5", true) }
-        tvSix.setOnClickListener { appendOnExpresstion("6", true) }
-        tvSeven.setOnClickListener { appendOnExpresstion("7", true) }
-        tvEight.setOnClickListener { appendOnExpresstion("8", true) }
-        tvNine.setOnClickListener { appendOnExpresstion("9", true) }
-        tvZero.setOnClickListener { appendOnExpresstion("0", true) }
+        tvSatu.setOnClickListener { appendOnExpresstion("1", true) }
+        tvDua.setOnClickListener { appendOnExpresstion("2", true) }
+        tvTiga.setOnClickListener { appendOnExpresstion("3", true) }
+        tvEmpat.setOnClickListener { appendOnExpresstion("4", true) }
+        tvLima.setOnClickListener { appendOnExpresstion("5", true) }
+        tvEnam.setOnClickListener { appendOnExpresstion("6", true) }
+        tvTujuh.setOnClickListener { appendOnExpresstion("7", true) }
+        tvDelapan.setOnClickListener { appendOnExpresstion("8", true) }
+        tvSembilan.setOnClickListener { appendOnExpresstion("9", true) }
+        tvNol.setOnClickListener { appendOnExpresstion("0", true) }
         tvDot.setOnClickListener { appendOnExpresstion(".", true) }
 
         //Operators
-        tvPlus.setOnClickListener { appendOnExpresstion("+", false) }
-        tvMinus.setOnClickListener { appendOnExpresstion("-", false) }
-        tvMul.setOnClickListener { appendOnExpresstion("*", false) }
+        tvTambah.setOnClickListener { appendOnExpresstion("+", false) }
+        tvKurang.setOnClickListener { appendOnExpresstion("-", false) }
+        tvKali.setOnClickListener { appendOnExpresstion("*", false) }
         tvDivide.setOnClickListener { appendOnExpresstion("/", false) }
         tvKuadrat2.setOnClickListener { appendOnExpresstion("^2", false) }
 
         tvClear.setOnClickListener {
-            tvExpression.text = ""
+            tvResult2.text = ""
             tvResult.text = ""
         }
 
         tvBack.setOnClickListener {
-            val string = tvExpression.text.toString()
+            val string = tvResult2.text.toString()
             if(string.isNotEmpty()){
-                tvExpression.text = string.substring(0,string.length-1)
+                tvResult2.text = string.substring(0,string.length-1)
             }
             tvResult.text = ""
         }
 
-        tvEquals.setOnClickListener {
+        tvSamaDgn.setOnClickListener {
             try {
 
-                val expression = ExpressionBuilder(tvExpression.text.toString()).build()
+                val expression = ExpressionBuilder(tvResult2.text.toString()).build()
                 val result = expression.evaluate()
                 val longResult = result.toLong()
                 if(result == longResult.toDouble())
@@ -67,15 +67,15 @@ class MainActivity : AppCompatActivity() {
     fun appendOnExpresstion(string: String, canClear: Boolean) {
 
         if(tvResult.text.isNotEmpty()){
-            tvExpression.text = ""
+            tvResult2.text = ""
         }
 
         if (canClear) {
             tvResult.text = ""
-            tvExpression.append(string)
+            tvResult2.append(string)
         } else {
-            tvExpression.append(tvResult.text)
-            tvExpression.append(string)
+            tvResult2.append(tvResult.text)
+            tvResult2.append(string)
             tvResult.text = ""
         }
     }
